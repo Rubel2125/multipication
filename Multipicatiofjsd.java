34827beae0a5a587660789ae1886b9eb8bcbeab2
anamul hoque <anamulhoque@anamuls-mbp.lan>
2019-01-12 22:32:45 -0500

    six multi

--- Multipicatiofjsd.java
+++ Multipicatiofjsd.java
@@ -6,6 +6,12 @@ public class Multipicatiofjsd {
 		for(int i=1;i<=10;i++) {
 			System.out.println("5" + "*" +i+ " = " +5*i);
 		}
+		System.out.println("-------------------------");
+		for(int i=1;i<=10;i++) {
+			System.out.println("6" + "*" +i+ " = " +6*i);
+		}
+		
+		
 	}
 	
 
